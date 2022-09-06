0c3c9cebf6b59c1d5c97cd922fbcc9fc8ec31ff2
Denchik23083 <deniskudravov228@gmail.com>
2022-09-06 08:42:59 +0300

    add gitignore and some fix with rendering books

--- TestBlazor/TestBlazor.Web/obj/Debug/net5.0/RazorDeclaration/PageComponents/Books/BookDetailsComponent.razor.g.cs
+++ TestBlazor/TestBlazor.Web/obj/Debug/net5.0/RazorDeclaration/PageComponents/Books/BookDetailsComponent.razor.g.cs
@@ -13,203 +13,203 @@ namespace TestBlazor.Web.PageComponents.Books
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 1 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 2 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 3 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 4 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 5 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 6 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 7 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 8 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 9 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using TestBlazor.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 10 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using TestBlazor.Web.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 11 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.Db.Entities;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 12 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.Logic;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 13 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.WebDb;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 14 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.Db.Entities.Books;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 15 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.Logic.BooksService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 16 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using TestBlazor.Web.Pages.TestModel1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 17 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 17 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.Logic.TestService;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 18 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 18 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using System.Timers;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 19 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 19 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using TestBlazor.Web.Utils;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 20 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 20 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using TestBlazor.Web.PageComponents.Books;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 21 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 21 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using TestBlazor.Web.PageComponents.TestModel1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 22 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 22 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using TestBlazor.Web.PageComponents.TestModel23;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 23 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 23 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.Db.Entities.TestModel23;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 24 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 24 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Blazor.Db.Entities.TestModel1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 25 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 25 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Tewr.Blazor.FileReader;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 26 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 26 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using System.IO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 27 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 27 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using System.Linq.Expressions;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 28 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 28 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using Markdig;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 29 "C:\Users\Lukian228\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
+#line 29 "C:\Users\User\source\repos\CSharp\TestBlazor\TestBlazor.Web\_Imports.razor"
 using System.Text.RegularExpressions;
 
 #line default
