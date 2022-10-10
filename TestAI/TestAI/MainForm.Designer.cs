4d9a6e7de610e38970adee0634730293d873b7f4
Denchik23083 <deniskudravov228@gmail.com>
2022-10-10 17:51:05 +0300

    Raw Test Karen

---
+++ TestAI/TestAI/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace TestAI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListCommands = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ListCommands
+            // 
+            this.ListCommands.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ListCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ListCommands.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ListCommands.ForeColor = System.Drawing.Color.Turquoise;
+            this.ListCommands.FormattingEnabled = true;
+            this.ListCommands.ItemHeight = 25;
+            this.ListCommands.Location = new System.Drawing.Point(0, 0);
+            this.ListCommands.Name = "ListCommands";
+            this.ListCommands.Size = new System.Drawing.Size(800, 450);
+            this.ListCommands.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListCommands);
+            this.Name = "MainForm";
+            this.Text = "Karen";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListCommands;
+    }
+}
+
---
+++ TestAI/TestAI/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace TestAI
+{
+    public partial class MainForm : Form
+    {
+        public readonly SpeechRecognitionEngine Recognizer = new SpeechRecognitionEngine();
+        public readonly SpeechRecognitionEngine StartListening = new SpeechRecognitionEngine();
+        public SpeechSynthesizer Karen = new SpeechSynthesizer();
+        public Random Rnd = new Random();
+        public int RecTimeOut = 0;
+        public DateTime TimeNow = DateTime.Now;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Recognizer.SetInputToDefaultAudioDevice();
+            Recognizer.LoadGrammarAsync(GetGrammar());
+            Recognizer.SpeechRecognized += Default_SpeechRecognition;
+            Recognizer.SpeechDetected += Recognizer_SpeechRecognition;
+            Recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            StartListening.SetInputToDefaultAudioDevice();
+            StartListening.LoadGrammarAsync(GetGrammar());
+            StartListening.SpeechRecognized += StartListening_SpeechRecognized;
+        }
+
+        private void Default_SpeechRecognition(object sender, SpeechRecognizedEventArgs e)
+        {
+            //int ranNum;
+
+            var mySpeech = e.Result.Text;
+
+            switch (mySpeech)
+            {
+                case "Who are you?":
+                    Karen.SpeakAsync("I am Karen.");
+                    break;
+                case "What time is it?":
+                    Karen.SpeakAsync(DateTime.Now.ToString("hh mm tt"));
+                    break;
+                case "What day is it?":
+                    Karen.SpeakAsync(DateTime.Now.DayOfWeek.ToString());
+                    break;
+                case "Show commands":
+                    Karen.SpeakAsync("OK");
+                    var choices = File.ReadAllLines(@"Commands.txt");
+                    foreach (var choice in choices)
+                    {
+                        ListCommands.Items.Add($"{choice}");
+                    }
+                    break;
+                case "Close":
+                    Close();
+                    break;
+            }
+        }
+
+        private void Recognizer_SpeechRecognition(object sender, SpeechDetectedEventArgs e)
+        {
+
+        }
+
+        private void StartListening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+        }
+
+        private Grammar GetGrammar()
+        {
+            var choices = new Choices(File.ReadAllLines(@"Commands.txt"));
+
+            var grammarBuilder = new GrammarBuilder(choices)
+            {
+                Culture = new CultureInfo("EN-US")
+            };
+
+            return new Grammar(grammarBuilder);
+        }
+    }
+}
