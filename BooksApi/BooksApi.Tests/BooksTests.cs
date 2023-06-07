bcf4f53c2ff23e978c6e876cc18fe57aac34d46c
Denchik23083 <deniskudravov228@gmail.com>
2023-06-07 16:45:41 +0300

    Testing is done

--- BooksApi/BooksApi.Tests/BooksTests.cs
+++ BooksApi/BooksApi.Tests/BooksTests.cs
@@ -1,5 +1,7 @@
-using System.Text.Json;
 using BooksApi.Db.Entities;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text.Json;
 using Xunit;
 
 namespace BooksApi.Tests
@@ -17,19 +19,104 @@ namespace BooksApi.Tests
         [Fact]
         public async Task Get_All_Books()
         {
-            var result = await _httpClient.GetAsync("/api/Books");
+            var expectedCount = 4;
 
-            var body = await result.Content.ReadAsStringAsync();
+            var body = await GetData("api/Books");
 
-            if (!result.IsSuccessStatusCode)
+            var books = JsonSerializer.Deserialize<IEnumerable<Book>>(body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            if (books is null)
+            {
+                throw new ArgumentNullException(nameof(books));
+            }
+            
+            Assert.NotNull(books);
+            Assert.Equal(expectedCount, books.Count());
+        }
+
+        [Fact]
+        public async Task Get_Book()
+        {
+            var expectedId = 1;
+
+            var body = await GetData($"api/Books/id?id={expectedId}");
+
+            var book = JsonSerializer.Deserialize<Book>(body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            if (book is null)
+            {
+                throw new ArgumentNullException(nameof(book));
+            }
+
+            Assert.NotNull(book);
+            Assert.Equal(expectedId, book.Id);
+        }
+
+        [Fact]
+        public async Task Create_Book()
+        {
+            var newBook = new Book
+            {
+                Title = "newTitle",
+                Author = "newAuthor",
+                PagesCount = 200,
+                PublishDate = new DateTime(2001, 03, 15),
+                CategoryId = 1
+            };
+
+            var content = JsonContent.Create(newBook);
+
+            var response = await _httpClient.PostAsync("api/Books", content);
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Update_Book()
+        {
+            var expectedId = 7;
+
+            var updatedBook = new Book
+            {
+                Title = "updatedTitle",
+                Author = "updatedAuthor",
+                PagesCount = 200,
+                PublishDate = DateTime.Now.AddYears(-2),
+                CategoryId = 1
+            };
+
+            var content = JsonContent.Create(updatedBook);
+
+            var response = await _httpClient.PutAsync($"api/Books/id?id={expectedId}", content);
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Delete_Book()
+        {
+            var expectedId = 7;
+            
+            var response = await _httpClient.DeleteAsync($"api/Books/id?id={expectedId}");
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        private async Task<string> GetData(string requestUrl)
+        {
+            var response = await _httpClient.GetAsync(requestUrl);
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
             {
                 throw new ApplicationException(body);
             }
 
-            var books = JsonSerializer.Deserialize<IEnumerable<Book>>(body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-            Assert.NotNull(books);
-            Assert.Equal(3, books!.Count());
+            return body;
         }
     }
 }
---
+++ BooksApi/BooksApi.Tests/CategoriesTests.cs
@@ -0,0 +1,114 @@
+﻿using BooksApi.Db.Entities;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text.Json;
+using Xunit;
+
+namespace BooksApi.Tests
+{
+    public class CategoriesTests : ApiTestBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoriesTests()
+        {
+            var app = new ApiTestBase();
+            _httpClient = app.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_All_Categories()
+        {
+            var expectedCount = 2;
+
+            var body = await GetData("api/Categories");
+
+            var categories = JsonSerializer.Deserialize<IEnumerable<Category>>(body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            if (categories is null)
+            {
+                throw new ArgumentNullException(nameof(categories));
+            }
+
+            Assert.NotNull(categories);
+            Assert.Equal(expectedCount, categories.Count());
+        }
+
+        [Fact]
+        public async Task Get_Category()
+        {
+            var expectedId = 1;
+
+            var body = await GetData($"api/Categories/id?id={expectedId}");
+
+            var category = JsonSerializer.Deserialize<Category>(body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            if (category is null)
+            {
+                throw new ArgumentNullException(nameof(category));
+            }
+
+            Assert.NotNull(category);
+            Assert.Equal(expectedId, category.Id);
+        }
+
+        [Fact]
+        public async Task Create_Category()
+        {
+            var newCategory = new Category
+            {
+                Description = "newDescription",
+            };
+
+            var content = JsonContent.Create(newCategory);
+
+            var response = await _httpClient.PostAsync("api/Categories", content);
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Update_Category()
+        {
+            var expectedId = 3;
+
+            var updatedCategory = new Category
+            {
+                Description = "updatedDescription"
+            };
+
+            var content = JsonContent.Create(updatedCategory);
+
+            var response = await _httpClient.PutAsync($"api/Categories/id?id={expectedId}", content);
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Delete_Category()
+        {
+            var expectedId = 3;
+
+            var response = await _httpClient.DeleteAsync($"api/Categories/id?id={expectedId}");
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        private async Task<string> GetData(string requestUrl)
+        {
+            var response = await _httpClient.GetAsync(requestUrl);
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(body);
+            }
+
+            return body;
+        }
+    }
+}
