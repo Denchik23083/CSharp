4d09b2d39aec6ff007d4779e8988df2b87875526
Denchik23083 <deniskudravov228@gmail.com>
2024-01-25 13:17:09 +0200

    SignalR template is done

---
+++ SignalR/SignalR.Web/Pages/Index.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace SignalR.Web.Pages
+{
+    public partial class Index : IAsyncDisposable
+    {
+        private HubConnection? hubConnection;
+        private List<string> messages = new();
+        private string? userInput;
+        private string? messageInput;
+
+        [Inject] private NavigationManager Navigation { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl(Navigation.ToAbsoluteUri("/chathub"))
+                .Build();
+
+            hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                var encodedMsg = $"{user}: {message}";
+                messages.Add(encodedMsg);
+                InvokeAsync(StateHasChanged);
+            });
+
+            await hubConnection.StartAsync();
+        }
+
+        private async Task Send()
+        {
+            if (hubConnection is not null)
+            {
+                await hubConnection.SendAsync("SendMessage", userInput, messageInput);
+            }
+        }
+
+        public bool IsConnected =>
+            hubConnection?.State == HubConnectionState.Connected;
+
+        public async ValueTask DisposeAsync()
+        {
+            if (hubConnection is not null)
+            {
+                await hubConnection.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ SignalR/SignalR.Web/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using SignalR.Web.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using SignalR.Web.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseResponseCompression();
+app.MapHub<ChatHub>("/chathub");
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
